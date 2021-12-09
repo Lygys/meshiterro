24d36ef66d14894a708c6fbe544c05c79f6a2dbb
EC2 Default User <ec2-user@ip-172-31-35-179.ec2.internal>
2021-12-09 02:10:43 +0000

    4章突入。マイグレート

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '76a524e2a7855d26ee4f2be40eb6d53e96fdee51169f3cfef704d8ce236bd09dd2306c53e2334feee41603337efdb5a1027615f590a80cac949c0fd09730f5bf'
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
